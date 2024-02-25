0468e9ca15af3ad5ed19f42726346d4517ad2576
hhw <2589622350@qq.com>
2024-02-25 20:10:37 +0800

    split SysY.g4 to Lexical and Syntax .g4, add docs

--- src/ASTPrinter.cpp
+++ src/ASTPrinter.cpp
@@ -5,4 +5,28 @@
 #include <iostream>
 using namespace std;
 #include "ASTPrinter.h"
-#include "SysYParser.h"
+#include "./.cpp/SysYParser.h"
+
+any ASTPrinter::visitNumber(SysYParser::NumberContext *ctx) {
+  cout << ctx->IntConst()->getText();
+  return nullptr;
+}
+
+any ASTPrinter::visitString(SysYParser::StringContext *ctx) {
+  cout << ctx->String()->getText();
+  return nullptr;
+}
+
+any ASTPrinter::visitFuncRParams(SysYParser::FuncRParamsContext *ctx) {
+  if (ctx->funcRParam().empty())
+    return nullptr;
+  auto numParams = ctx->funcRParam().size();
+  ctx->funcRParam(0)->accept(this);
+  for (int i = 1; i < numParams; ++i) {
+    cout << ", ";
+    ctx->funcRParam(i)->accept(this);
+  }
+  cout << '\n';
+  return nullptr;
+}
+
