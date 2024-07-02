a92c9a7bce20acd705df7b324ee5fdc9e5c2c877
xinchen <zexinjian@foxmail.com>
2024-07-02 14:56:58 +0800

    [update]update the code

--- test.c
+++ test.c
@@ -1,9 +1,19 @@
-int fun(int a[]) {
-  return 0;
-}
+// int fun(int a[], int b) {
+//   a[1] = 2;
+//   return 0;
+// }
 
-int main(){
-  int a[2];
-  a[1] = 23;
-  return fun(a);
+// int main(){
+//   int a[2];
+//   int b = 2;
+//   a[1] = 23;
+//   return fun(a, b);
+// }
+
+int main() {
+  int a[3];
+  int b = 2;
+  a[2] = 2;
+
+  return a[2];
 }
