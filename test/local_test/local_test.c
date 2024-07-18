a583c4df65616a80c90381068fe0d99b4612553b
westme10n <3104854549@qq.com>
2024-07-18 20:59:23 +0800

    lcssa unfinished

--- test/local_test/local_test.c
+++ test/local_test/local_test.c
@@ -1,21 +1,13 @@
-int g = 0;
-
-int func(int n) {
-  g = g + n;
-  putint(g);
-  return g;
+int func(int a){
+    int i = 0;
+    int b;
+    while (i < 100){
+        b = a + 2;
+        i+=1;
+    }
+    return b;
 }
-
-int main() {
-  int i;
-  i = getint();
-  if (i > 10 && func(i)) i = 1; else i = 0;
-  i = getint();
-  if (i > 11 && func(i)) i = 1; else i = 0;
-  i = getint();
-  if (i <= 99 || func(i)) i = 1; else i = 0;
-  i = getint();
-  if (i <= 100 || func(i)) i = 1; else i = 0;
-  if (!func(99) && func(100)) i = 1; else i = 0;
-  return 0;
+int main(){
+    func(3);
+    return 0;
 }
