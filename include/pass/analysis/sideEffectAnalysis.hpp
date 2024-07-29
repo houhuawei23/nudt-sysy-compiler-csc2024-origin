3eb8a7cc4eca91fec5315b00d04e085551e2c86e
三进制冒险家 <1508812553@qq.com>
2024-07-29 12:20:07 +0800

    [add]1. 加入findGlobalVariable 到module中。 2. 修复SEA的一些问题 3. 修复islib的一些问题

--- include/pass/analysis/sideEffectAnalysis.hpp
+++ include/pass/analysis/sideEffectAnalysis.hpp
@@ -4,9 +4,12 @@ using namespace pass;
 
 class sideEffectAnalysis:public ModulePass{
     public:
-    void run(ir::Module* md,TopAnalysisInfoManager* tp)override;
-    void infoCheck(ir::Module* md);
+        void run(ir::Module* md,TopAnalysisInfoManager* tp)override;
+        std::string name() const override;
+    
     private:
-    callGraph* cgctx;
-    sideEffectInfo* sectx;
+        callGraph* cgctx;
+        sideEffectInfo* sectx;
+        void infoCheck(ir::Module* md);
+        bool isGlobal(ir::GetElementPtrInst* gep);
 };
