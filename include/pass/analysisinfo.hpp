fe73bedf8195309af4e45b9a300ce0b61d0320fd
westme10n <3104854549@qq.com>
2024-08-03 16:23:46 +0800

    finish licm

--- include/pass/analysisinfo.hpp
+++ include/pass/analysisinfo.hpp
@@ -24,8 +24,7 @@ class analysisInfo {
     bool isValid;
 
   public:
-    analysisInfo(PassUnit* mp, TopAnalysisInfoManager* mtp, bool v = false)
-      : isValid(v), passUnit(mp), topManager(mtp) {}
+    analysisInfo(PassUnit* mp, TopAnalysisInfoManager* mtp, bool v = false) : isValid(v), passUnit(mp), topManager(mtp) {}
     void setOn() { isValid = true; }
     void setOff() { isValid = false; }
     virtual void refresh() = 0;
@@ -41,40 +40,26 @@ class domTree : public FunctionACtx {
     std::unordered_map<ir::BasicBlock*, ir::BasicBlock*> _sdom;
     std::unordered_map<ir::BasicBlock*, int> _domlevel;
     std::unordered_map<ir::BasicBlock*, std::vector<ir::BasicBlock*>> _domson;
-    std::unordered_map<ir::BasicBlock*, std::vector<ir::BasicBlock*>>
-      _domfrontier;
+    std::unordered_map<ir::BasicBlock*, std::vector<ir::BasicBlock*>> _domfrontier;
     std::vector<ir::BasicBlock*> _BFSDomTreeVector;
     std::vector<ir::BasicBlock*> _DFSDomTreeVector;
 
   public:
-    domTree(ir::Function* func, TopAnalysisInfoManager* tp)
-      : FunctionACtx(func, tp) {}
+    domTree(ir::Function* func, TopAnalysisInfoManager* tp) : FunctionACtx(func, tp) {}
     ir::BasicBlock* idom(ir::BasicBlock* bb) { return _idom[bb]; }
-    void set_idom(ir::BasicBlock* bb, ir::BasicBlock* idbb) {
-        _idom[bb] = idbb;
-    }
+    void set_idom(ir::BasicBlock* bb, ir::BasicBlock* idbb) { _idom[bb] = idbb; }
     ir::BasicBlock* sdom(ir::BasicBlock* bb) { return _sdom[bb]; }
-    void set_sdom(ir::BasicBlock* bb, ir::BasicBlock* sdbb) {
-        _sdom[bb] = sdbb;
-    }
+    void set_sdom(ir::BasicBlock* bb, ir::BasicBlock* sdbb) { _sdom[bb] = sdbb; }
     int domlevel(ir::BasicBlock* bb) { return _domlevel[bb]; }
     void set_domlevel(ir::BasicBlock* bb, int lv) { _domlevel[bb] = lv; }
 
-    std::vector<ir::BasicBlock*>& domson(ir::BasicBlock* bb) {
-        return _domson[bb];
-    }
+    std::vector<ir::BasicBlock*>& domson(ir::BasicBlock* bb) { return _domson[bb]; }
 
-    std::vector<ir::BasicBlock*>& domfrontier(ir::BasicBlock* bb) {
-        return _domfrontier[bb];
-    }
+    std::vector<ir::BasicBlock*>& domfrontier(ir::BasicBlock* bb) { return _domfrontier[bb]; }
 
-    std::vector<ir::BasicBlock*>& BFSDomTreeVector() {
-        return _BFSDomTreeVector;
-    }
+    std::vector<ir::BasicBlock*>& BFSDomTreeVector() { return _BFSDomTreeVector; }
 
-    std::vector<ir::BasicBlock*>& DFSDomTreeVector() {
-        return _DFSDomTreeVector;
-    }
+    std::vector<ir::BasicBlock*>& DFSDomTreeVector() { return _DFSDomTreeVector; }
 
     void clearAll() {
         _idom.clear();
@@ -123,24 +108,24 @@ class domTree : public FunctionACtx {
         }
     }
     void DFSDomTreeInfoRefresh() {
-      std::stack<ir::BasicBlock*> bbstack;
-      std::unordered_map<ir::BasicBlock*, bool> vis;
-      for (auto bb : passUnit->blocks())
-          vis[bb] = false;
+        std::stack<ir::BasicBlock*> bbstack;
+        std::unordered_map<ir::BasicBlock*, bool> vis;
+        for (auto bb : passUnit->blocks())
+            vis[bb] = false;
 
-      _DFSDomTreeVector.clear();
-      bbstack.push(passUnit->entry());
+        _DFSDomTreeVector.clear();
+        bbstack.push(passUnit->entry());
 
-      while (!bbstack.empty()) {
-          auto bb = bbstack.top();
-          bbstack.pop();
-          if (!vis[bb]) {
-              _DFSDomTreeVector.push_back(bb);
-              vis[bb] = true;
-              for (auto bbDomSon : _domson[bb])
-                  bbstack.push(bbDomSon);
-          }
-      }
+        while (!bbstack.empty()) {
+            auto bb = bbstack.top();
+            bbstack.pop();
+            if (!vis[bb]) {
+                _DFSDomTreeVector.push_back(bb);
+                vis[bb] = true;
+                for (auto bbDomSon : _domson[bb])
+                    bbstack.push(bbDomSon);
+            }
+        }
     }
 };
 
@@ -150,28 +135,18 @@ class pdomTree : public FunctionACtx {  // also used as pdom
     std::unordered_map<ir::BasicBlock*, ir::BasicBlock*> _spdom;
     std::unordered_map<ir::BasicBlock*, int> _pdomlevel;
     std::unordered_map<ir::BasicBlock*, std::vector<ir::BasicBlock*>> _pdomson;
-    std::unordered_map<ir::BasicBlock*, std::vector<ir::BasicBlock*>>
-      _pdomfrontier;
+    std::unordered_map<ir::BasicBlock*, std::vector<ir::BasicBlock*>> _pdomfrontier;
 
   public:
-    pdomTree(ir::Function* func, TopAnalysisInfoManager* tp)
-      : FunctionACtx(func, tp) {}
+    pdomTree(ir::Function* func, TopAnalysisInfoManager* tp) : FunctionACtx(func, tp) {}
     ir::BasicBlock* ipdom(ir::BasicBlock* bb) { return _ipdom[bb]; }
-    void set_ipdom(ir::BasicBlock* bb, ir::BasicBlock* idbb) {
-        _ipdom[bb] = idbb;
-    }
+    void set_ipdom(ir::BasicBlock* bb, ir::BasicBlock* idbb) { _ipdom[bb] = idbb; }
     ir::BasicBlock* spdom(ir::BasicBlock* bb) { return _spdom[bb]; }
-    void set_spdom(ir::BasicBlock* bb, ir::BasicBlock* sdbb) {
-        _spdom[bb] = sdbb;
-    }
+    void set_spdom(ir::BasicBlock* bb, ir::BasicBlock* sdbb) { _spdom[bb] = sdbb; }
     int pdomlevel(ir::BasicBlock* bb) { return _pdomlevel[bb]; }
     void set_pdomlevel(ir::BasicBlock* bb, int lv) { _pdomlevel[bb] = lv; }
-    std::vector<ir::BasicBlock*>& pdomson(ir::BasicBlock* bb) {
-        return _pdomson[bb];
-    }
-    std::vector<ir::BasicBlock*>& pdomfrontier(ir::BasicBlock* bb) {
-        return _pdomfrontier[bb];
-    }
+    std::vector<ir::BasicBlock*>& pdomson(ir::BasicBlock* bb) { return _pdomson[bb]; }
+    std::vector<ir::BasicBlock*>& pdomfrontier(ir::BasicBlock* bb) { return _pdomfrontier[bb]; }
     void clearAll() {
         _ipdom.clear();
         _spdom.clear();
@@ -186,6 +161,17 @@ class pdomTree : public FunctionACtx {  // also used as pdom
             _pdomfrontier[bb] = std::vector<ir::BasicBlock*>();
         }
     }
+
+    bool pdominate(ir::BasicBlock* bb1, ir::BasicBlock* bb2) {
+        if (bb1 == bb2) return true;
+        auto bbIdom = _ipdom[bb2];
+        while (bbIdom != nullptr) {
+            if (bbIdom == bb1) return true;
+            bbIdom = _ipdom[bbIdom];
+        }
+        return false;
+    }
+
     void refresh() override;
 };
 
@@ -196,16 +182,13 @@ class loopInfo : public FunctionACtx {
     std::unordered_map<ir::BasicBlock*, size_t> _looplevel;
 
   public:
-    loopInfo(ir::Function* fp, TopAnalysisInfoManager* tp)
-      : FunctionACtx(fp, tp) {}
+    loopInfo(ir::Function* fp, TopAnalysisInfoManager* tp) : FunctionACtx(fp, tp) {}
     std::vector<ir::Loop*>& loops() { return _loops; }
     ir::Loop* head2loop(ir::BasicBlock* bb) {
         if (_head2loop.count(bb) == 0) return nullptr;
         return _head2loop[bb];
     }
-    void set_head2loop(ir::BasicBlock* bb, ir::Loop* lp) {
-        _head2loop[bb] = lp;
-    }
+    void set_head2loop(ir::BasicBlock* bb, ir::Loop* lp) { _head2loop[bb] = lp; }
     int looplevel(ir::BasicBlock* bb) { return _looplevel[bb]; }
     void set_looplevel(ir::BasicBlock* bb, int lv) { _looplevel[bb] = lv; }
     void clearAll() {
@@ -221,9 +204,7 @@ class loopInfo : public FunctionACtx {
                 if (innermost == nullptr)
                     innermost = L;
                 else {
-                    if (_looplevel[L->header()] <
-                        _looplevel[innermost->header()])
-                        innermost = L;
+                    if (_looplevel[L->header()] < _looplevel[innermost->header()]) innermost = L;
                 }
             }
         }
@@ -241,14 +222,9 @@ class callGraph : public ModuleACtx {
     std::unordered_map<ir::Function*, bool> _is_lib;
 
   public:
-    callGraph(ir::Module* md, TopAnalysisInfoManager* tp)
-      : ModuleACtx(md, tp) {}
-    std::set<ir::Function*>& callees(ir::Function* func) {
-        return _callees[func];
-    }
-    std::set<ir::Function*>& callers(ir::Function* func) {
-        return _callers[func];
-    }
+    callGraph(ir::Module* md, TopAnalysisInfoManager* tp) : ModuleACtx(md, tp) {}
+    std::set<ir::Function*>& callees(ir::Function* func) { return _callees[func]; }
+    std::set<ir::Function*>& callers(ir::Function* func) { return _callers[func]; }
     bool isCalled(ir::Function* func) { return _is_called[func]; }
     bool isInline(ir::Function* func) { return _is_inline[func]; }
     bool isLib(ir::Function* func) { return _is_lib[func]; }
@@ -268,12 +244,12 @@ class callGraph : public ModuleACtx {
             _callers[func] = std::set<ir::Function*>();
         }
     }
-    bool isNoCallee(ir::Function* func) { 
-      if(_callees[func].size()==0)return true;
-      for(auto f:_callees[func]){
-        if(not isLib(f))return false;
-      }
-      return true;
+    bool isNoCallee(ir::Function* func) {
+        if (_callees[func].size() == 0) return true;
+        for (auto f : _callees[func]) {
+            if (not isLib(f)) return false;
+        }
+        return true;
     }
     void refresh() override;
 };
@@ -283,8 +259,7 @@ class indVarInfo : public FunctionACtx {
     std::unordered_map<ir::Loop*, ir::indVar*> _loopToIndvar;
 
   public:
-    indVarInfo(ir::Function* fp, TopAnalysisInfoManager* tp)
-      : FunctionACtx(fp, tp) {}
+    indVarInfo(ir::Function* fp, TopAnalysisInfoManager* tp) : FunctionACtx(fp, tp) {}
     ir::indVar* getIndvar(ir::Loop* loop) {
         if (_loopToIndvar.count(loop) == 0) return nullptr;
         return _loopToIndvar[loop];
@@ -296,22 +271,19 @@ class indVarInfo : public FunctionACtx {
 
 class sideEffectInfo : public ModuleACtx {
   private:
-    std::unordered_map<ir::Function*,bool> _hasSideEffect;
-    std::unordered_map<ir::Function*,bool> _hasGlobalVariableUse;
+    std::unordered_map<ir::Function*, bool> _hasSideEffect;
+    std::unordered_map<ir::Function*, bool> _hasGlobalVariableUse;
 
   public:
-    sideEffectInfo(ir::Module* ctx, TopAnalysisInfoManager* tp)
-      : ModuleACtx(ctx, tp) {}
+    sideEffectInfo(ir::Module* ctx, TopAnalysisInfoManager* tp) : ModuleACtx(ctx, tp) {}
     void clearAll() {
-       _hasSideEffect.clear();  
-       _hasGlobalVariableUse.clear();
+        _hasSideEffect.clear();
+        _hasGlobalVariableUse.clear();
     }
     void refresh() override;
-    void setFuncSideEffect(ir::Function* func,bool b){_hasSideEffect[func]=b;}
-    bool hasSideEffect(ir::Function* func){return _hasSideEffect[func];}
-    void setFuncGVUse(ir::Function* func,bool b){_hasGlobalVariableUse[func]=b;}
-    bool isPureFunc(ir::Function* func){return not _hasGlobalVariableUse[func] and not _hasSideEffect[func];}
-
-
+    void setFuncSideEffect(ir::Function* func, bool b) { _hasSideEffect[func] = b; }
+    bool hasSideEffect(ir::Function* func) { return _hasSideEffect[func]; }
+    void setFuncGVUse(ir::Function* func, bool b) { _hasGlobalVariableUse[func] = b; }
+    bool isPureFunc(ir::Function* func) { return not _hasGlobalVariableUse[func] and not _hasSideEffect[func]; }
 };
 };  // namespace pass
---
+++ src/pass/optimize/licm.cpp
@@ -0,0 +1,150 @@
+#include "pass/optimize/optimize.hpp"
+#include "pass/optimize/licm.hpp"
+#include "ir/value.hpp"
+#include <set>
+#include <cassert>
+#include <map>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+namespace pass {
+ir::Value* LICM::getbase(ir::Instruction* inst) {
+    ir::Value* ptr;
+    if (inst->dynCast<ir::StoreInst>())
+        ptr = inst->dynCast<ir::StoreInst>()->ptr();
+    else if (inst->dynCast<ir::LoadInst>())
+        ptr = inst->dynCast<ir::LoadInst>()->ptr();
+    if (!ptr->dynCast<ir::GetElementPtrInst>()) return ptr;
+    auto gep = ptr->dynCast<ir::GetElementPtrInst>();
+    while (true) {
+        if (!gep->value()->dynCast<ir::GetElementPtrInst>()) return gep->value();
+        gep = gep->value()->dynCast<ir::GetElementPtrInst>();
+    }
+}
+
+bool LICM::alias(ir::Instruction* inst0, ir::Instruction* inst1) {
+    if (inst0->dynCast<ir::CallInst>()) {
+        return true;
+    }
+    if (inst0->dynCast<ir::LoadInst>() || inst0->dynCast<ir::StoreInst>()) {
+        auto instbase = getbase(inst0);
+        auto storebase = getbase(inst1);
+        if (instbase == storebase) return true;  // 存疑？？
+        return false;
+    } else {
+        return false;
+    }
+}
+
+bool LICM::isinvariantop(ir::Instruction* inst, ir::Loop* loop) {
+    for (auto op : inst->operands()) {
+        if (auto opinst = op->value()->dynCast<ir::Instruction>()) {
+            if (loop->contains(opinst->block())) return false;
+        }
+    }
+    return true;
+}
+
+bool LICM::checkload(ir::StoreInst* storeinst, ir::Loop* loop) {
+    for (auto bb : loop->blocks()) {
+        for (auto inst : bb->insts()) {
+            if (inst->dynCast<ir::LoadInst>()) {
+                if (alias(inst, storeinst)){
+                    auto storebb = storeinst->block();
+                    auto loadbb = inst->block();
+                    if (storebb != loadbb){
+                        if (domctx->dominate(loadbb, storebb))
+                            return false;
+                    }
+                    else{
+                        auto storeit = std::find(storebb->insts().begin(), storebb->insts().end(), storeinst);
+                        auto loadit = std::find(loadbb->insts().begin(), loadbb->insts().end(), inst);
+                        int storeidx = std::distance(storebb->insts().begin(), storeit);
+                        int loadidx = std::distance(loadbb->insts().begin(), loadit);
+                        if (storeidx > loadidx) return false;
+                    }
+                } 
+            }
+        }
+    }
+    return true;
+}
+
+bool LICM::checkstore(ir::LoadInst* loadinst, ir::Loop* loop) {
+    for (auto bb : loop->blocks()) {
+        for (auto inst : bb->insts()) {
+            if (inst->dynCast<ir::StoreInst>()) {
+                if (alias(inst, loadinst)) return false;
+            }
+            else if (inst->dynCast<ir::CallInst>()) {
+                auto callee = inst->dynCast<ir::CallInst>()->callee();
+                if (sectx->hasSideEffect(callee)){
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+std::vector<ir::Instruction*> LICM::getinvariant(ir::BasicBlock* bb, ir::Loop* loop) {
+    std::vector<ir::Instruction*> res;
+    auto head = loop->header();
+    ir::BasicBlock* headnext;
+    for (auto bb : head->next_blocks()){
+        if (loop->contains(bb)){
+            headnext = bb;
+            break;
+        } 
+    }
+    if (!pdomcctx->pdominate(bb,headnext)) return res;
+    for (auto inst : bb->insts()) {
+        if (auto storeinst = inst->dynCast<ir::StoreInst>()) {
+            if (isinvariantop(storeinst, loop))
+                if (checkload(storeinst, loop)){  // 接下来检查循环体里有无对本数组的load
+                    res.push_back(storeinst);
+                    std::cerr<<"lift store"<<std::endl;
+                }
+        
+        } else if (auto loadinst = inst->dynCast<ir::LoadInst>()) {
+            if (isinvariantop(loadinst, loop))
+                if (checkstore(loadinst, loop)){  // 接下来检查循环体里有无对本数组的store
+                    res.push_back(loadinst);
+                    std::cerr<<"lift load"<<std::endl;
+                }
+        }
+    }
+    return res;
+}
+
+void LICM::run(ir::Function* func, TopAnalysisInfoManager* tp) {
+    loopctx = tp->getLoopInfo(func);
+    loopctx->refresh();
+    domctx = tp->getDomTree(func);
+    domctx->refresh();
+    pdomcctx = tp->getPDomTree(func);
+    pdomcctx->refresh();
+    sectx = tp->getSideEffectInfo();
+    sectx->setOff();
+    sectx->refresh();
+    for (auto loop : loopctx->loops()) {
+        auto preheader = loop->getLoopPreheader();
+        bool changed = true;
+        while (changed) {
+            std::vector<ir::Instruction*> inststoremove;
+            for (auto bb : loop->blocks()) {
+                std::vector<ir::Instruction*> inststoremoveinbb;
+                inststoremoveinbb = getinvariant(bb, loop);
+                inststoremove.insert(inststoremove.end(), inststoremoveinbb.begin(), inststoremoveinbb.end());
+            }
+            changed = (inststoremove.size() > 0);
+            for (auto inst : inststoremove) {
+                auto instbb = inst->block();
+                instbb->move_inst(inst);
+                preheader->emplace_lastbutone_inst(inst);
+            }
+        }
+    }
+}
+}  // namespace pass
