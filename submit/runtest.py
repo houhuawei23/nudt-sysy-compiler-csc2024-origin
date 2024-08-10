f1fdd08e5ea7cbe7b5e7353a19ab7ddfc8209b7f
hhw <2589622350@qq.com>
2024-08-10 17:02:26 +0800

    [clean the working directory]

--- submit/runtest.py
+++ submit/runtest.py
@@ -46,7 +46,7 @@ def functionalTest():
         sysy_runtime,
         sysy_link_for_riscv_gpp,
     )
-    test.set("riscv", "2024", functional_timeout, 1, 0)
+    test.set("riscv", "2024", functional_timeout, 0, 0)
     test.runFunctionalTest("functional")
     # test.run("hidden_functional")
     # test.run_single_case("functional", "00_main.sy")
